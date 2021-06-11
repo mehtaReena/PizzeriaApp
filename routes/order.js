f9e1e181a4e5986d943209c1322797133832bf43
mehtaReena <j.reena07@gmail.com>
2021-06-11 11:20:19 +0530

    commit

--- routes/order.js
+++ routes/order.js
@@ -9,5 +9,17 @@ router
 
 })
 
+router.get('/:BookID' ,async (req,res ,next)=>{
+    console.log(req.params)
+    let id =req.params.BookID
+    let bookList = await bookController.removeBookByID(id)
+     let List = await bookController.printAllBooks()
+     res.json(List);
+
+     res.redirect('/');
+     next();
+
+})
+
 
 module.exports= router;
